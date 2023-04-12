0b293e9935b879388d597671ea9fa8a2552f61e9
Amy Kaminski <amykaminski928@gmail.com>
2023-04-12 12:07:11 -0500

    make migrations for comment

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from authentication.models import User
 
-class Comment(models.model):
+class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=255)
     text = models.CharField(max_length=255)
