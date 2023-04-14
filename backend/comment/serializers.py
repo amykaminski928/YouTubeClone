d40822348a29b77f73d198bb55b820eae6f2b1df
Amy Kaminski <amykaminski928@gmail.com>
2023-04-14 15:07:59 -0500

    added serializer and view for comments by video

--- backend/comment/serializers.py
+++ backend/comment/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework_simplejwt import serializers
 from .models import Comment
 
-class CommentSerializer(serializers.modelserializer)
+class CommentSerializer(serializers.modelserializer):
     class Meta:
         model = Comment
         fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
+        depth = 1
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+@api_view (['GET'])
+@permission_classes([AllowAny])
+def get_comments_by_video(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
